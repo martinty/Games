8c95d220bb387566dd819c79cf39d32b472f67c8
Martin <martinty@users.noreply.github.com>
2020-05-06 12:40:51 +0200

    Add cash and bet to game

--- blackjack/Blackjack.cpp
+++ blackjack/Blackjack.cpp
@@ -1,6 +1,7 @@
 #include "Blackjack.h"
 
 #include <FL/Fl.H>
+#include <FL/fl_ask.H>
 #include <FL/fl_draw.H>
 
 #include <iomanip>
@@ -9,21 +10,36 @@
 Blackjack::Blackjack() : Fl_Widget{0, 0, screenWidth, screenHeight} {
     deck.shuffle();
     gameInfo->color(FL_GRAY, FL_GRAY);
-    gameInfo->buffer(textBuff);
+    gameInfo->buffer(gameTextBuff);
     gameInfo->label("Blackjack game info");
     gameInfo->hide();
+    cashInfo->color(FL_GRAY, FL_GRAY);
+    cashInfo->buffer(cashTextBuff);
+    cashInfo->label("Blackjack cash info");
+    updateCashInfo();
+    cashInput->maximum_size(4);
+    hidePlayerBtn();
+    yesBtn->hide();
+    noBtn->hide();
 }
 
 void Blackjack::init() {
-    fl_draw("New round?", w() / 2 - 30, cardHeight * 5 - 20);
-    if (yesBtn->click()) {
-        newRound();
-    } else if (noBtn->click()) {
-        exit(0);
+    if (Fl::event_key(FL_Enter) && cashInput->position()) {
+        stringstream ss{cashInput->value()};
+        int number;
+        ss >> number;
+        cashInput->cut(0, cashInput->maximum_size());
+        if (number > 0 && number <= playerCash) {
+            bet = number;
+            playerCash -= bet;
+            cashInput->hide();
+            updateCashInfo();
+            startRound();
+        }
     }
 }
 
-void Blackjack::newRound() {
+void Blackjack::startRound() {
     rounds++;
     if (deck.size() < 20) newDeck();
     for (int i{0}; i < 2; i++) {
@@ -31,25 +47,31 @@ void Blackjack::newRound() {
         dealer.newCard(deck.drawCard());
     }
     dealer.setCardState(1, CardState::back);
+    showPlayerBtn();
     stage = Stage::player;
 }
 
 void Blackjack::playerTurn() {
     if (player.getBlackjack()) {
         stage = Stage::dealer;
-        return;
-    }
-    fl_draw("New card?", w() / 2 - 30, cardHeight * 5 - 20);
-    if (yesBtn->click()) {
+    } else if (hitBtn->click()) {
         player.newCard(deck.drawCard());
         if (player.getValue() == 21) {
             stage = Stage::dealer;
         } else if (player.getBusted()) {
-            showGameInfo();
             stage = Stage::showdown;
         }
-    } else if (noBtn->click()) {
+    } else if (standBtn->click()) {
         stage = Stage::dealer;
+    } else if (doubleDownBtn->click() && bet <= playerCash) {
+        playerCash -= bet;
+        bet += bet;
+        player.newCard(deck.drawCard());
+        if (player.getBusted()) {
+            stage = Stage::showdown;
+        } else {
+            stage = Stage::dealer;
+        }
     }
 }
 
@@ -58,73 +80,117 @@ void Blackjack::dealerTurn() {
            (dealer.getSoft() && dealer.getValue() == 17)) {
         dealer.newCard(deck.drawCard());
     }
-    showGameInfo();
     stage = Stage::showdown;
 }
 
 void Blackjack::showdown() {
-    dealer.setCardState(1, CardState::front);
-    fl_draw("Continue?", w() / 2 - 30, cardHeight * 5 - 20);
+    if (!gameInfo->visible()) {
+        hidePlayerBtn();
+        updateGameInfo();
+        updateCashInfo();
+        gameInfo->show();
+        yesBtn->show();
+        noBtn->show();
+        dealer.setCardState(1, CardState::front);
+    }
+    fl_draw("New round?", w() / 2 - 30, cardHeight * 5 - 20);
     if (yesBtn->click()) {
         dealer.reset();
         player.reset();
         gameInfo->hide();
+        yesBtn->hide();
+        noBtn->hide();
+        cashInput->show();
+        bet = 0;
+        updateCashInfo();
+        if (playerCash <= 0) {
+            fl_message("You're out of cash... The game will now exit.");
+            exit(0);
+        }
         stage = Stage::init;
     } else if (noBtn->click()) {
+        fl_message("Thanks for playing Blackjack! The game will now exit.");
         exit(0);
     }
 }
 
-void Blackjack::showGameInfo() {
+void Blackjack::updateGameInfo() {
     string info;
     if (player.getBlackjack()) {
         if (dealer.getBlackjack()) {
             info = "Both got Blackjack, it's a push!\n\n";
             playerPoints += 0.5;
             dealerPoints += 0.5;
+            playerCash += bet;
         } else {
             info = "Player won with Blackjack!\n\n";
             playerPoints += 1;
+            playerCash += bet * 2;
+            dealerCash -= bet;
         }
     } else if (dealer.getBlackjack()) {
         info = "Dealer won with Blackjack!\n\n";
         dealerPoints += 1;
+        dealerCash += bet;
     } else if (player.getBusted()) {
         info = "Dealer won, player got busted!\n\n";
         dealerPoints += 1;
+        dealerCash += bet;
     } else if (dealer.getBusted()) {
         info = "Player won, dealer got busted!\n\n";
         playerPoints += 1;
+        playerCash += bet * 2;
+        dealerCash -= bet;
     } else if (player.getValue() < dealer.getValue()) {
         info =
             "Dealer won with value " + to_string(dealer.getValue()) + "!\n\n";
         dealerPoints += 1;
+        dealerCash += bet;
     } else if (player.getValue() > dealer.getValue()) {
         info =
             "Player won with value " + to_string(player.getValue()) + "!\n\n";
         playerPoints += 1;
+        playerCash += bet * 2;
+        dealerCash -= bet;
     } else {
         info = "Equal value, it's a push!\n\n";
         playerPoints += 0.5;
         dealerPoints += 0.5;
+        playerCash += bet;
     }
-
     string dealerPct = to_string((int)std::round(dealerPoints / rounds * 100));
     string playerPct = to_string((int)std::round(playerPoints / rounds * 100));
     ostringstream ssPlayer, ssDealer;
     ssDealer << fixed << setprecision(1) << dealerPoints;
     ssPlayer << fixed << setprecision(1) << playerPoints;
-    info += "Rounds: " + to_string(rounds) + "\n";
-    info += "Dealer's points: " + ssDealer.str() + "\t" + dealerPct + "%\n";
-    info += "Player's points: " + ssPlayer.str() + "\t" + playerPct + "%\n";
-    textBuff->text(info.c_str());
+    info += "Rounds: " + to_string(rounds) + "\n" +
+            "Dealer's points: " + ssDealer.str() + "\t" + dealerPct + "%\n" +
+            "Player's points: " + ssPlayer.str() + "\t" + playerPct + "%";
+    gameTextBuff->text(info.c_str());
+}
 
-    gameInfo->show();
+void Blackjack::updateCashInfo() {
+    string info = "Current bet: " + to_string(bet) + "\n\n" +
+                  "Dealer's cash: " + to_string(dealerCash) + "\n" +
+                  "Player's cash: " + to_string(playerCash);
+    cashTextBuff->text(info.c_str());
 }
 
 void Blackjack::newDeck() {
     deck.reset();
     deck.shuffle();
+}
+
+void Blackjack::showPlayerBtn() {
+    hitBtn->show();
+    standBtn->show();
+    doubleDownBtn->show();
+}
+
+void Blackjack::hidePlayerBtn() {
+    hitBtn->hide();
+    standBtn->hide();
+    doubleDownBtn->hide();
 }
 
 void Blackjack::playGame() {
--- blackjack/Blackjack.h
+++ blackjack/Blackjack.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <FL/Fl_Int_Input.H>
 #include <FL/Fl_Round_Clock.H>
 #include <FL/Fl_Text_Display.H>
 #include <FL/Fl_Widget.H>
@@ -20,24 +21,41 @@ class Blackjack : public Fl_Widget {
     double dealerPoints = 0;
     double playerPoints = 0;
     int rounds = 0;
+    int dealerCash = 10000;
+    int playerCash = 3000;
+    int bet = 0;
     Stage stage = Stage::init;
     void init();
-    void newRound();
+    void startRound();
     void playerTurn();
     void dealerTurn();
     void showdown();
-    void showGameInfo();
+    void updateGameInfo();
+    void updateCashInfo();
     void newDeck();
+    void showPlayerBtn();
+    void hidePlayerBtn();
     void playGame();
     void draw() override;
 
+    unique_ptr<Fl_Text_Display> gameInfo =
+        make_unique<Fl_Text_Display>(40, (int)(cardHeight * 4.5), 250, 100);
+    unique_ptr<Fl_Text_Display> cashInfo = make_unique<Fl_Text_Display>(
+        w() - 290, (int)(cardHeight * 4.5), 250, 100);
+    Fl_Text_Buffer *gameTextBuff = new Fl_Text_Buffer();
+    Fl_Text_Buffer *cashTextBuff = new Fl_Text_Buffer();
     unique_ptr<Button> yesBtn = make_unique<Button>(
         screenWidth / 2 - 80, cardHeight * 5, 40, 20, "Yes");
     unique_ptr<Button> noBtn =
         make_unique<Button>(screenWidth / 2 + 40, cardHeight * 5, 40, 20, "No");
-    unique_ptr<Fl_Text_Display> gameInfo =
-        make_unique<Fl_Text_Display>(40, (int)(cardHeight * 4.5), 250, 110);
-    Fl_Text_Buffer *textBuff = new Fl_Text_Buffer();
+    unique_ptr<Button> hitBtn =
+        make_unique<Button>(w() / 2 - 170, cardHeight * 5, 100, 20, "Hit");
+    unique_ptr<Button> standBtn =
+        make_unique<Button>(w() / 2 - 50, cardHeight * 5, 100, 20, "Stand");
+    unique_ptr<Button> doubleDownBtn = make_unique<Button>(
+        w() / 2 + 70, cardHeight * 5, 100, 20, "Double down");
+    unique_ptr<Fl_Int_Input> cashInput = make_unique<Fl_Int_Input>(
+        w() / 2 - 30, cardHeight * 5 - 20, 60, 20, "Bet");
     unique_ptr<Fl_Round_Clock> clock =
         make_unique<Fl_Round_Clock>(screenWidth - 100, 20, 80, 80);
 
